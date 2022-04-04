ccf801c46e889ea98aebcd7e2951392448f20e9e
fujii noritsugu <quercus491@gmail.com>
2022-04-05 07:33:40 +0900

    リモートでの共同開発

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+struct Pom{
+    p:u8,
+    q:Qom
+}
+
+enum Qom{
+    Qom1,
+    Qom2
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{}",pom());
+    let pom = Pom{
+        p:3,
+        q:Qom::Qom1
+    };
+
 }
 
 fn pom() -> &'static str {
